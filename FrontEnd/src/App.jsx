8b564216e0a320af673e61435b23cb52969bc0da
piyushd3112 <dhonglep@gmail.com>
2024-10-29 13:45:02 +0530

    created navbar

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import NavBar from "./Components/Navbar/NavBar";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="app">
+      <NavBar />
+    </div>
+  );
+};
 
-export default App
+export default App;
