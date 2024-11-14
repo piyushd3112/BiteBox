7527b8ae745c5e5dc3b0f5a2e9deea6081f5a6c2
piyushd3112 <dhonglep@gmail.com>
2024-11-14 13:46:54 +0530

    created explore menu section

---
+++ FrontEnd/src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+          sed quia rerum sint consequatur itaque aperiam, a quasi,
+          necessitatibus at qui asperiores praesentium totam nobis, illum hic
+          nesciunt ut saepe?
+        </p>
+        <button>View Menu </button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
